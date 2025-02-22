8595471dec982956f192468c0f8b7ad6dcba0b96
Michael Holloway <mdh@mdholloway.org>
2025-02-22 01:22:24 +0000

    Use Boost

--- 0_echo.cpp
+++ 0_echo.cpp
@@ -1,64 +1,40 @@
+#include <boost/asio.hpp>
 #include <iostream>
-#include <cstring>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
 #include <vector>
 
+using boost::asio::ip::tcp;
+
 int main() {
-    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd < 0) {
-        std::cerr << "Failed to create socket\n";
-        return 1;
-    }
+    try {
+        boost::asio::io_context io_context;
+        tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 5000));
 
-    int opt = 1;
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
-        std::cerr << "Failed to set socket options\n";
-        return 1;
-    }
+        std::cout << "Server listening on port 5000\n";
 
-    sockaddr_in address;
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(5000);
+        while (true) {
+            tcp::socket socket(io_context);
+            acceptor.accept(socket);
 
-    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
-        std::cerr << "Failed to bind\n";
-        return 1;
-    }
+            std::vector<char> buffer(1024);
+            boost::system::error_code error;
 
-    if (listen(server_fd, 10) < 0) {
-        std::cerr << "Failed to listen\n";
-        return 1;
-    }
+            while (true) {
+                size_t length = socket.read_some(boost::asio::buffer(buffer), error);
 
-    std::cout << "Server listening on port 5000\n";
+                if (error == boost::asio::error::eof) {
+                    break;
+                } else if (error) {
+                    throw boost::system::system_error(error);
+                }
 
-    while (true) {
-        sockaddr_in client_addr;
-        socklen_t client_len = sizeof(client_addr);
-        int client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &client_len);
-        
-        if (client_fd < 0) {
-            std::cerr << "Failed to accept connection\n";
-            continue;
-        }
-
-        std::vector<char> buffer(1024);
-        ssize_t bytes_read;
-
-        while ((bytes_read = read(client_fd, buffer.data(), buffer.size())) > 0) {
-            if (write(client_fd, buffer.data(), bytes_read) != bytes_read) {
-                std::cerr << "Failed to write back all data\n";
-                break;
+                boost::asio::write(socket, boost::asio::buffer(buffer, length));
             }
         }
-
-        close(client_fd);
+    } catch (std::exception& e) {
+        std::cerr << "Exception: " << e.what() << "\n";
+        return 1;
     }
 
-    close(server_fd);
     return 0;
 }
 
